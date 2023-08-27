08528caf8d0992021ed3f31791fc6c7712ff64b7
Maxim Goryachkin <maxgoryachkin@mail.ru>
2023-08-27 12:31:43 +0300

    Initial commit

---
+++ BestRecipes/Application/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  BestRecipes
+//
+//  Created by Максим Горячкин on 27.08.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let rootVC = ViewController()
+        window.rootViewController = rootVC
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+
+}
+
---
+++ BestRecipes/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestRecipes
+//
+//  Created by Максим Горячкин on 27.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
