63d95db5be2bc122b7b5f28a5e67d537a6d9b40d
Ivan Biriukov <Grottt0@mail.ru>
2023-08-29 01:23:24 +0300

    Add SearchBar to Home VC; Configure SearchBar and its delegates Create Extension for SearchBar and UIImage;

--- BestRecipes/View+Presenter/HomeViewController.swift
+++ BestRecipes/View+Presenter/HomeViewController.swift
@@ -41,14 +41,31 @@ class HomeViewController: UIViewController {
         return lb
     }()
     
+    private let searchBar : UISearchBar = {
+        let bar = UISearchBar()
+        bar.placeholder = "Search recipes"
+        bar.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 40).isActive = true
+        bar.searchBarStyle = .minimal
+        bar.searchTextField.layer.borderColor = UIColor.neutral20.cgColor
+        bar.searchTextField.layer.borderWidth = 2
+        bar.searchTextField.layer.cornerRadius = 8
+        bar.searchTextField.leftView?.tintColor = .neutral100
+        bar.searchTextField.font = .poppinsRegular16
+        return bar
+    }()
+    
     // MARK: - LifeCycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         addSubviews()
         setupConstraints()
+        setupSearchBar()
+        
 
     }
+    
     
     // MARK: - ConfigureUI
 
@@ -57,10 +74,21 @@ class HomeViewController: UIViewController {
         scrollView.addSubview(contentView)
         contentView.addSubview(contentStackView)
         contentStackView.addArrangedSubview(titleLabel)
+        contentStackView.addArrangedSubview(searchBar)
     }
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            contentStackView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16),
+            contentStackView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16),
+            titleLabel.leadingAnchor.constraint(equalTo: contentStackView.leadingAnchor),
+        ])
+    }
+    
+    private func setupSearchBar() {
+        searchBar.delegate = self
+        searchBar.updateHeight(height: 48)
     }
     
     
@@ -69,3 +97,18 @@ class HomeViewController: UIViewController {
 
 
 // MARK: - TableView Delegate & DataSource
+
+
+
+// MARK: - SearchBar Delegates
+
+extension HomeViewController : UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.endEditing(true)
+    }
+    
+}
+
+
