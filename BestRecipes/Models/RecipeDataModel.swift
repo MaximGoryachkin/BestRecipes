ef0ae7e285bbabbc890c257c041de2d9dfb21bce
Ivan Biriukov <Grottt0@mail.ru>
2023-08-31 21:57:04 +0300

    Model create is done;

---
+++ BestRecipes/Models/RecipeDataModel.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct RecipeDataModel {
+    let recipeId : Int
+    let recipeImage : String?
+    let recipeRating : Float?
+    let cookDuration : String?
+    let recipeTitle : String?
+    let authorAvatar : String?
+    let authorName : String?
+    let isSavedToFavorite : Bool?
+}
