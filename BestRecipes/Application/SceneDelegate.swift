99baafa1f904c1c4acb98b66c7f78866a14dd942
Evgenii Mazrukho <mazrukho@gmail.com>
2023-09-01 15:14:10 +0300

    sceneDelegate

--- BestRecipes/Application/SceneDelegate.swift
+++ BestRecipes/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let rootVC = TestViewController()
+        let rootVC = OnboardingPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
         window.rootViewController = rootVC
         window.makeKeyAndVisible()
         self.window = window
