3a8199648ad6c6bb48e38c9d9e46656271dd9857
Ilyas Tyumenev <tumenoff@mail.ru>
2023-09-07 17:45:41 +0300

    Update frames for buttons

--- BestRecipes/View+Presenter/TabBarController/TabBarController.swift
+++ BestRecipes/View+Presenter/TabBarController/TabBarController.swift
@@ -7,12 +7,15 @@
 
 import UIKit
 
-class TabBarController: UITabBarController, UITabBarControllerDelegate {
+class TabBarController: UITabBarController {
     
     // MARK: - Properties
+    private let kBarHeight: CGFloat = 120
+    
     private lazy var plusButton : UIButton = {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 48, height: 48))
         button.setBackgroundImage(UIImage(named: "createRecipeButton"), for: .normal)
+        button.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
         return button
     }()
     
@@ -21,7 +24,7 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         super.viewDidLoad()
         setupItems()
         plusButton.frame = CGRect(x: Int(self.tabBar.bounds.width)/2 - Int(plusButton.bounds.width)/2,
-                                  y: -34,
+                                  y: 0,
                                   width: 48,
                                   height: 48)
     }
@@ -32,17 +35,30 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         setupCustomTabBar()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        tabBar.frame.size.height = kBarHeight
+        tabBar.frame.origin.y = view.frame.height - kBarHeight
+    }
+    
+    // MARK: - Target actions
+    @objc func plusButtonTapped() {
+        let createRecipeViewController = CreateRecipeViewController()
+        createRecipeViewController.modalPresentationStyle = .automatic
+        present(createRecipeViewController, animated: true, completion: nil)
+    }
+    
     // MARK: - Private Methods
     private func setupCustomTabBar() {
         let shape = CAShapeLayer()
         shape.path = createPath()
         shape.fillColor = UIColor.white.cgColor
         shape.lineWidth = 8.0
-        
         shape.shadowOffset = CGSize(width: 0, height: -1)
         shape.shadowColor = UIColor(red: 108/255, green: 108/255, blue: 108/255, alpha: 0.08).cgColor
         shape.shadowOpacity = 1
         self.tabBar.layer.insertSublayer(shape, at: 0)
+        self.tabBar.itemWidth = 40
     }
     
     private func setupItems() {
@@ -54,7 +70,8 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         bookmarks.tabBarItem.image = UIImage(named: "Bookmark/Inactive")
         bookmarks.tabBarItem.selectedImage = UIImage(named: "Bookmark/Active")
         
-        let createRecipe = CreateRecipeViewController()
+        let createRecipe = UIViewController()
+        createRecipe.tabBarItem.isEnabled = false
         
         let notifications = UIViewController()
         notifications.tabBarItem.image = UIImage(named: "Notification/Inactive")
@@ -65,11 +82,10 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         profile.tabBarItem.image = UIImage(named: "Profile/Inactive")
         profile.tabBarItem.selectedImage = UIImage(named: "Profile/Active")
         
-        home.tabBarItem.imageInsets = UIEdgeInsets(top: -7, left: 15, bottom: 7, right: -15)
-        bookmarks.tabBarItem.imageInsets = UIEdgeInsets(top: -7, left: 3, bottom: 7, right: -3)
-        createRecipe.tabBarItem.imageInsets = UIEdgeInsets(top: -35, left: -5, bottom: 32, right: -5)
-        notifications.tabBarItem.imageInsets = UIEdgeInsets(top: -7, left: -3, bottom: 7, right: 3)
-        profile.tabBarItem.imageInsets = UIEdgeInsets(top: -7, left: -15, bottom: 7, right: 15)
+        home.tabBarItem.imageInsets = UIEdgeInsets(top: 16, left: 15, bottom: -16, right: -15)
+        bookmarks.tabBarItem.imageInsets = UIEdgeInsets(top: 16, left: 3, bottom: -16, right: -3)
+        notifications.tabBarItem.imageInsets = UIEdgeInsets(top: 16, left: -3, bottom: -16, right: 3)
+        profile.tabBarItem.imageInsets = UIEdgeInsets(top: 16, left: -15, bottom: -16, right: 15)
         
         setViewControllers([home, bookmarks, createRecipe, notifications, profile], animated: true)
     }
@@ -80,23 +96,23 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         let maxX: CGFloat = self.tabBar.bounds.width
         
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: minX, y: -20))
-        path.addLine(to: CGPoint(x: centerX - 54.9, y: -20))
-        path.addCurve(to: CGPoint(x: centerX - 32.9, y: -4),
-                      controlPoint1: CGPoint(x: centerX - 45.1, y: -20),
-                      controlPoint2: CGPoint(x: centerX - 36.9, y: -12.9))
-        path.addCurve(to: CGPoint(x: centerX, y: 21),
-                      controlPoint1: CGPoint(x: centerX - 27.7, y: 7.5),
-                      controlPoint2: CGPoint(x: centerX - 17.8, y: 21))
-        path.addCurve(to: CGPoint(x: centerX + 32.9, y: -4),
-                      controlPoint1: CGPoint(x: centerX + 17.8, y: 21),
-                      controlPoint2: CGPoint(x: centerX + 27.7, y: 7.5))
-        path.addCurve(to: CGPoint(x: centerX + 54.9, y: -20),
-                      controlPoint1: CGPoint(x: centerX + 36.9, y: -12.9),
-                      controlPoint2: CGPoint(x: centerX + 45.1, y: -20))
-        path.addLine(to: CGPoint(x: maxX, y: -20))
-        path.addLine(to: CGPoint(x: maxX, y: 120))
-        path.addLine(to: CGPoint(x: minX, y: 120))
+        path.move(to: CGPoint(x: minX, y: 14))
+        path.addLine(to: CGPoint(x: centerX - 54.9, y: 14))
+        path.addCurve(to: CGPoint(x: centerX - 32.9, y: 30),
+                      controlPoint1: CGPoint(x: centerX - 45.1, y: 14),
+                      controlPoint2: CGPoint(x: centerX - 36.9, y: 21.1))
+        path.addCurve(to: CGPoint(x: centerX, y: 55),
+                      controlPoint1: CGPoint(x: centerX - 27.7, y: 41.5),
+                      controlPoint2: CGPoint(x: centerX - 17.8, y: 55))
+        path.addCurve(to: CGPoint(x: centerX + 32.9, y: 30),
+                      controlPoint1: CGPoint(x: centerX + 17.8, y: 55),
+                      controlPoint2: CGPoint(x: centerX + 27.7, y: 41.5))
+        path.addCurve(to: CGPoint(x: centerX + 54.9, y: 14),
+                      controlPoint1: CGPoint(x: centerX + 36.9, y: 21.1),
+                      controlPoint2: CGPoint(x: centerX + 45.1, y: 14))
+        path.addLine(to: CGPoint(x: maxX, y: 14))
+        path.addLine(to: CGPoint(x: maxX, y: kBarHeight))
+        path.addLine(to: CGPoint(x: minX, y: kBarHeight))
         path.addLine(to: CGPoint(x: minX, y: -20))
         path.close()
         return path.cgPath
